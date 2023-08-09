c983205457c40b49ec7da9d1b29b74e9c3b98107
gomshiki <gomshiki@gmail.com>
2023-08-09 11:29:53 +0900

    firebase sms 문자보내기 완료및 전화번호 자동 하이픈 기능 구현

--- src/app/signIn/page.jsx
+++ src/app/signIn/page.jsx
@@ -1,41 +1,71 @@
 "use client"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { signInWithPhoneNumber, RecaptchaVerifier } from "firebase/auth"
 import { auth } from "../firebase";
-
-const setUpRecaptcha = () =>{
-    auth.languageCode = 'ko';
-    window.recaptchaVerifier = new RecaptchaVerifier(auth, 'sign-in-button', {
-        'size': 'invisible',
-        'callback': (response) => {
-          // reCAPTCHA solved, allow signInWithPhoneNumber.
-          onSignInSubmit();
-        }
-      });
-}
-
-
-const onSignInSubmit = (event) =>{
-    event.preventDefault();
-    setUpRecaptcha();
-    const phoneNumber = '+8201034592299';
-    const appVerifier = window.recaptchaVerifier;
-
-    signInWithPhoneNumber(auth, phoneNumber, appVerifier)
-    .then((confirmationResult) => {
-        window.confirmationResult = confirmationResult;
-        console.log(confirmationResult);
-    }).catch((error) => {
-        console.log(error);
-    });
-}
 
 
 
 
 export default function SignIn(){
 
-    const [phonenumber, setPhonenumber] = useState("");
+    const [phoneNumber, setphoneNumber] = useState("");
+    const [authNumber, setAuthNumber] = useState("");
+
+    // useEffect : 마운트 시 호출되고, phoneNumber가 변경되어 재랜더링 될따만 호출됨
+    useEffect(() => {
+        if (phoneNumber.length === 10) {
+            setphoneNumber(phoneNumber.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3'));
+        }
+        if (phoneNumber.length === 13) {
+            setphoneNumber(phoneNumber.replace(/-/g, '').replace(/(\d{3})(\d{4})(\d{4})/, '$1-$2-$3'));
+        }
+      }, [phoneNumber]);
+
+    const activeEnter = (e)=>{
+        if(e.key == "Enter"){
+            onSignInSubmit(event);
+        }
+    }
+
+    const setUpRecaptcha = () =>{
+        auth.languageCode = 'ko';
+        window.recaptchaVerifier = new RecaptchaVerifier(auth, 'sign-in-button', {
+            'size': 'invisible',
+            'callback': (response) => {
+                onSignInSubmit();
+            }
+          });
+    }
+
+    const handleChange = (e)=>{
+        const regex = /^[0-9\b -]{0,13}$/;
+        if (regex.test(e.target.value)) {
+          setphoneNumber(e.target.value);
+        }
+    }
+    
+    
+    const onSignInSubmit = (event) =>{
+    
+        event.preventDefault();
+    
+        setUpRecaptcha();
+
+        const rawPhoneNumber = '+82' + phoneNumber.replaceAll("-","");
+        const appVerifier = window.recaptchaVerifier;
+
+        console.log('rawPhoneNumber',rawPhoneNumber);
+        
+        signInWithPhoneNumber(auth, rawPhoneNumber, appVerifier)
+        .then((confirmationResult) => {
+            window.confirmationResult = confirmationResult;
+            console.log(confirmationResult);
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+    
+
 
     return(
         <div className="bg-white">
@@ -44,14 +74,17 @@ export default function SignIn(){
                     <h1 className='mb-2 text-lg font-medium text-center'>번호 인증</h1>
                     <form>
                         <div className="relative">
-                        <input id="phone" type="text" value={phonenumber} onChange={e=>setPhonenumber(e.target.value)} placeholder="전화번호 입력" className="float-left w-4/5 p-3 my-2 border bg-zinc-100 rounded-lg border-gray-400 bg-gray-50 trasition focus:bg-white hover:bg-white"/>
+                            <input type="text" value={phoneNumber} onChange={handleChange} onKeyDown={(e) => activeEnter(e)} placeholder="010-0000-0000" className="float-left w-4/5 p-3 my-2 border bg-zinc-100 rounded-lg border-gray-400 bg-gray-50 trasition focus:bg-white hover:bg-white"/>
                             <button onClick={onSignInSubmit} type="submit" id="sign-in-button" className="w-1/5 p-3 my-2 mb-1 text-xs font-bold text-white border rounded-3xl float-right bg-blue-400">
                                 번호요청
                             </button>
                         </div>
                     </form>
                     <form>
-            
+                        <input type="text" value={authNumber} onKeyDown={(e) => activeEnter(e)} placeholder="인증번호를 입력하세요" className=" w-full p-3 my-2 border bg-zinc-100 rounded-lg border-gray-400 bg-gray-50 trasition focus:bg-white hover:bg-white"/>
+                        <button onClick={onSignInSubmit} type="submit" id="sign-in-button" className=" w-full p-3 my-2 mb-1 text-xs font-bold text-white border rounded-3xl  bg-blue-400">
+                            다음
+                        </button>
 
                     </form>
                 </div>
